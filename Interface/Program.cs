b2c8a7452931efcea3d139fc46734b19d953136d
pawankamdi <pawankamdi26@gmail.com>
2023-02-25 15:28:12 +0530

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        public interface Istudent
+        {
+            void putdata();
+        }
+        public class student : Istudent
+        {
+            public int rollnumber;
+            public string name;
+            public student(int r, string n)
+            {
+                rollnumber = r;
+                name = n;
+            }
+            public void putdata()
+            {
+                Console.WriteLine("Student Roll number = {0}", rollnumber);
+                Console.WriteLine("Student name is : {0}", name);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            student s1 = new student(1,"pawan");
+           
+            s1.putdata();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Practise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    class Program
+    {
+        public static class sum
+        {
+            public static int staticvar = 5;
+            public static void call()
+            {
+                Console.WriteLine("Inside static method");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Call is :", sum.staticvar);
+            sum.call();
+            Console.ReadLine();
+        }
+    }
+}
