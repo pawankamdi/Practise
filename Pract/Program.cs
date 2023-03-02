fc8b45f02c3b3a7d2cb0b85a87ac4b46c5671a20
pawankamdi <pawankamdi26@gmail.com>
2023-03-02 15:24:15 +0530

    Extra Example

---
+++ Pract/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the Number from 1 to 100;
+            //for (int i = 1; i<=100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+
+
+            //Reverse the string
+            string str = " ", reverse = " ";
+            Console.WriteLine("Please Entre the string");
+            str = Console.ReadLine();
+            int Lenght;
+            Lenght = str.Length - 1;
+            while(Lenght >= 0)
+            {
+                reverse = reverse + str[Lenght];
+                Lenght--;
+            }
+            Console.WriteLine("Reverse string is : {0}", reverse);
+            Console.ReadLine();
+        }
+    }
+}
